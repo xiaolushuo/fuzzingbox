dd17a48e25b637344bd527d5d590f69f5aa2e154
flagellantX <37841951+flagellantX@users.noreply.github.com>
2018-11-05 23:16:35 +0800

    Update fuzzing.py

--- fuzzing.py
+++ fuzzing.py
@@ -8,54 +8,6 @@ from time import sleep
 
 def runWebTest():
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    print("fuzzing - 无前提下直接进行黑盒,选择你的目标:  \n 1: 谷歌 \n 2: 火狐 \n 3: 洋葱路由")
-    browserType = input('>>')
-    timeout = input(
-        "设置响应时间，建议区间30以上:")
-    checkValidBrowserType(browserType)
-    for root, folders, fileNames in os.walk("recurve"):
-        for fileName in fileNames:
-            if not fileName.endswith('.html'):
-                continue
-            processCommand = getBrowserApplication(browserType)
-            if processCommand is not None:
-                setupExploit(dir_path, fileName, processCommand, root)
-                runExploit(processCommand, timeout)
-            else:
-                print "无法对这类浏览器进行黑盒!"
-
-def runExploit(processCommand, timeout):
-    print "执行命令: " + " ".join(processCommand)
-    process = subprocess.Popen(processCommand)
-    sleep(timeout)
-    print "关闭黑盒浏览器进程"
-    process.kill()
-    sleep(3)
-
-
-def setupExploit(dir_path, fileName, processCommand, root):
-    filePath = os.path.join(dir_path, root, fileName)
-    filePath = "file://" + filePath
-    print "黑盒测试使用:" + filePath
-    processCommand.append(filePath)
-
-
-def getBrowserApplication(browserType):
-    if browserType == 1:
-        processCommand = ['google-chrome']
-    elif browserType == 2:
-        processCommand = ['firefox', '--new-instance']
-    elif browserType == 3:
-        processCommand = ['#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-
-import os
-import subprocess
-import sys
-from time import sleep
-
-def runWebTest():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
     print("fuzzing - 无前提下直接进行黑盒,选择你的目标:  \n 1: 谷歌 \n 2: 火狐 \n 3: 告诉我你想测试的浏览器，我进行添加，这并不难")
     browserType = input('>>')
     timeout = input(
